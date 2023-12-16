c18df14d50ba3234eca2c2b9405770c30ccbcafe
patrikduch <duchpatrik@icloud.com>
2023-12-16 19:43:08 +0100

    feat: graphql integration

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
 import { fastify } from "fastify";
+import mercurius from "mercurius";
 import pino from "pino";
 import test from "./tes";
+import { renderPlaygroundPage } from "graphql-playground-html";
+import { schema } from "./graphql/schema";
 
 const Port = process.env.PORT || 7000;
 const server = fastify({
   logger: pino({ level: "info" }),
 });
 
-// register plugin below:
+const resolvers = {
+  Query: {
+    hello: () => "Hello World!",
+  },
+};
+
+server.get("/playground", (_, reply) => {
+  reply.header("Content-Type", "text/html");
+  reply.send(renderPlaygroundPage({ endpoint: "/graphql" }));
+});
+
 server.register(test);
+
+server.register(mercurius, {
+  schema,
+  resolvers,
+});
 
 const start = async () => {
   try {
